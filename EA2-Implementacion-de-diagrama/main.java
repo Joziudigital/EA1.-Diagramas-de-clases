c48545f325e694c1a59409720a99de63a9f3373c
Jorge Zapata <jorge.zapatas@est.iudigital.edu.co>
2026-09-19 23:17:44 -0500

    Main

--- EA2-Implementacion-de-diagrama/main.java
+++ EA2-Implementacion-de-diagrama/main.java
@@ -1,3 +1,15 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
 public class main {
+    private static final long MILLIS_POR_DIA = 24 * 60 * 60 * 1000;
+
+    public static void main(String[] args) {
+        
+        Usuario usuario1 = new Usuario("", "
     
 }
