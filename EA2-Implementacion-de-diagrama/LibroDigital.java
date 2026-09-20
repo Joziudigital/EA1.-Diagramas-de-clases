fb75eb063a96b18f044890289099da0b872e6f42
Jorge Zapata <jorge.zapatas@est.iudigital.edu.co>
2026-09-20 15:55:54 -0500

    act

--- EA2-Implementacion-de-diagrama/LibroDigital.java
+++ EA2-Implementacion-de-diagrama/LibroDigital.java
@@ -1,5 +1,79 @@
 
+import java.util.Date;
 
-public class LibroDigital {
 
+public class LibroDigital extends Libro {
+
+    private String formato;              // PDF, EPUB, MOBI, etc.
+    private double tamanoMB;
+    private int licenciasSimultaneas;    // cuántas personas pueden tenerlo abierto a la vez
+    private int licenciasEnUso;
+
+    public LibroDigital(String numeroSerie, String titulo, int cantidad, Date fechaPublicacion,
+                         Autor autor, String formato, double tamanoMB, int licenciasSimultaneas) {
+        super(numeroSerie, titulo, cantidad, fechaPublicacion, autor);
+        this.formato = formato;
+        this.tamanoMB = tamanoMB;
+        this.licenciasSimultaneas = licenciasSimultaneas;
+        this.licenciasEnUso = 0;
+    }
+
+
+    @Override
+    public boolean estaDisponible() {
+        return licenciasEnUso < licenciasSimultaneas;
+    }
+
+    public boolean descargar() {
+        return descargar(this.formato);
+    }
+
+    public boolean descargar(String formatoSolicitado) {
+        if (!estaDisponible()) {
+            return false;
+        }
+        licenciasEnUso++;
+        System.out.println("Descargando \"" + getTitulo() + "\" en formato " + formatoSolicitado + "...");
+        return true;
+    }
+
+    public void liberarLicencia() {
+        if (licenciasEnUso > 0) {
+            licenciasEnUso--;
+        }
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public double getTamanoMB() {
+        return tamanoMB;
+    }
+
+    public void setTamanoMB(double tamanoMB) {
+        this.tamanoMB = tamanoMB;
+    }
+
+    public int getLicenciasSimultaneas() {
+        return licenciasSimultaneas;
+    }
+
+    public void setLicenciasSimultaneas(int licenciasSimultaneas) {
+        this.licenciasSimultaneas = licenciasSimultaneas;
+    }
+
+    public int getLicenciasEnUso() {
+        return licenciasEnUso;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroDigital{" + getTitulo() + ", formato=" + formato +
+                ", licencias=" + licenciasEnUso + "/" + licenciasSimultaneas + "}";
+    }
 }
